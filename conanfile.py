1e281bc3b6be92bb2869fbcdb62f4cafd62ab2f2
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2017-05-02 08:37:24 +0300

    Bump zlib

--- conanfile.py
+++ conanfile.py
@@ -108,7 +108,7 @@ class BoostConan(ConanFile):
                 self.requires("bzip2/1.0.6@lasote/stable")
                 if not self.options.header_only:
                     self.options["bzip2/1.0.6"].shared = self.options.shared
-            self.requires("zlib/1.2.9@lasote/stable")
+            self.requires("zlib/1.2.11@lasote/stable")
             if not self.options.header_only:
                 self.options["zlib"].shared = self.options.shared
 
